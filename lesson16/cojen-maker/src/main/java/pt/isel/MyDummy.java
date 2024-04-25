ae7a0b521e614e7de3742786f6ea68e7c8af53dd
Luís Falcão <lfalcao@cc.isel.ipl.pt>
2024-04-25 12:23:53 +0100

    Lesson 16:

---
+++ lesson16/cojen-maker/src/main/java/pt/isel/MyDummy.java
@@ -0,0 +1,17 @@
+package pt.isel;
+
+public class MyDummy {
+    private final int nr;
+
+    public MyDummy(int n) {
+        super();
+        nr = n;
+    }
+
+    public int mul(int n) {
+        return this.nr * n;
+
+//        int res = this.nr * n;
+//        return res;
+    }
+}
